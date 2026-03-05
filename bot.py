3529249933b9b5338c01d41de4bb6498fd26c590
Ramiro R <ramiro.rela@gmail.com>
2026-03-05 15:00:50 -0300

    Fixed handling of reactions

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, MessageReactionHandler, ContextTypes
 import os
 
 TOKEN = os.environ["TOKEN"]
 
 async def anti_manito(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message and update.message.reaction:
-        for r in update.message.reaction:
-            if r.emoji == "👍":
-                await update.effective_chat.send_message("SIN MANITOOO 🪚")
+    for r in update.message_reaction.new_reaction:
+        if r.emoji == "👍":
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="SIN MANITOOO 🪚"
+            )
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.ALL, anti_manito))
-app.run_polling()
+app.add_handler(MessageReactionHandler(anti_manito))
+app.run_polling(allowed_updates=["message_reaction"])
